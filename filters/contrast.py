ce8bef62a4d4ad53053a71ee5b5bbe09d5718f89
tavares1 <lucastavaresvasconcelos@gmail.com>
2018-08-05 21:42:19 -0300

    Corrigindo alguns comentarios de codigo

--- filters/contrast.py
+++ filters/contrast.py
@@ -8,9 +8,9 @@ def constrat(data,value_contrast):
     height, width = data.shape
     for x in range(0, height):
         for y in range(0, width):
-            # Calculando o valor máximo de contrante do pixel, caso o valor calculado seja maior que 255, retornamos o 255, valor máximo do pixel.
+            # Calculando o valor máximo de contraste do pixel, caso o valor calculado seja maior que 255, retornamos o 255, valor máximo do pixel.
             max_contrast = data[x][y] + data[x][y] * 10 / 100 if data[x][y] + data[x][y] * 10 / 100 < 255 else 255
-            # Calculando o valor mínimo de contrante do pixelm, caso o valor calculado seja menor que 0, retornamos 0, valor minimo.
+            # Calculando o valor mínimo de contraste do pixel, caso o valor calculado seja menor que 0, retornamos 0, valor minimo.
             min_contrast = data[x][y] - data[x][y] * 10 / 100 if data[x][y] - data[x][y] * 10 / 100 > 0 else 0
             # Caso o valor do pixel, seja menor que o valor limiar de contraste, retornamos o valor minimo de contraste, senão retornamos o valor máximo.
             data_final[x][y] = min_contrast if data[x,y] < value_contrast else max_contrast
